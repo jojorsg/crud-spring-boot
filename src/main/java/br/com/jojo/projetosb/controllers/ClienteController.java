f3ca835666bf7cc313ca769563e1431e6a87d517
JoJo-Rob <josueroberto099@gmail.com>
2023-07-24 11:59:12 -0300

    Adicionando arquivos projeto-sb

---
+++ src/main/java/br/com/jojo/projetosb/controllers/ClienteController.java
@@ -0,0 +1,16 @@
+package br.com.jojo.projetosb.controllers;
+
+import br.com.jojo.projetosb.models.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+
+    @GetMapping(path = "/qualquer")
+    public Cliente obterCliente() {
+        return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+}
