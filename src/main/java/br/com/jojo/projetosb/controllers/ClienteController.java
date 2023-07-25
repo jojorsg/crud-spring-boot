5b1d36058c9e0940d39cc05851731c2f975a2f87
JoJo-Rob <josueroberto099@gmail.com>
2023-07-25 16:57:16 -0300

    Adicionando novas classes de controllers

--- src/main/java/br/com/jojo/projetosb/controllers/ClienteController.java
+++ src/main/java/br/com/jojo/projetosb/controllers/ClienteController.java
@@ -1,9 +1,7 @@
 package br.com.jojo.projetosb.controllers;
 
 import br.com.jojo.projetosb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/clientes")
@@ -12,5 +10,15 @@ public class ClienteController {
     @GetMapping(path = "/qualquer")
     public Cliente obterCliente() {
         return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Maria", "987.654.321-00");
+    }
+
+    @GetMapping
+    public Cliente obterClientePorId2(@RequestParam(name = "id") int id) {
+        return new Cliente(id, "João Augusto", "111.222.333-44");
     }
 }
