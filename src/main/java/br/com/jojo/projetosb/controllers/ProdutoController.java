a56545714da101e92f5ccf4e52a396c3748778e9
JoJo-Rob <josueroberto099@gmail.com>
2023-07-27 15:13:40 -0300

    Finalizando CRUD da entidade Produto

--- src/main/java/br/com/jojo/projetosb/controllers/ProdutoController.java
+++ src/main/java/br/com/jojo/projetosb/controllers/ProdutoController.java
@@ -3,7 +3,15 @@ package br.com.jojo.projetosb.controllers;
 import br.com.jojo.projetosb.model.entities.Produto;
 import br.com.jojo.projetosb.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -15,7 +23,7 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
     public @ResponseBody Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
@@ -26,8 +34,24 @@ public class ProdutoController {
         return produtoRepository.findAll();
     }
 
+    @GetMapping(path = "/nome/{parteNome}")
+    public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+        return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+    }
+
+    @GetMapping(path = "/pagina/{numeroPagina}")
+    public Iterable<Produto> obterPorProdutosPorPagina(@PathVariable int numeroPagina) {
+        Pageable page = PageRequest.of(numeroPagina, 3);
+        return produtoRepository.findAll(page);
+    }
+
     @GetMapping(path = "/{id}")
     public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
         return produtoRepository.findById(id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void excluirProduto(@PathVariable int id) {
+        produtoRepository.deleteById(id);
     }
 }
--- src/main/java/br/com/jojo/projetosb/model/repositories/ProdutoRepository.java
+++ src/main/java/br/com/jojo/projetosb/model/repositories/ProdutoRepository.java
@@ -1,8 +1,9 @@
 package br.com.jojo.projetosb.model.repositories;
 
 import br.com.jojo.projetosb.model.entities.Produto;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
+    public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 }
